39a5f5b590dbbd799448ac632711ea72c87cbf5e
Javan Wang <javan.wang@goalrecorder.com>
2017-01-08 00:09:49 -0500

    content population and added return to top button

--- app/js/main.js
+++ app/js/main.js
@@ -99,15 +99,35 @@ function main () {
         if (pages[newPage]) {
             removeClass(pages[newPage], 'hidden');
             addClass(pages[newPage], 'active');
+            $('body,html').animate({
+                scrollTop : 0                       // Scroll to top of body
+            }, 10);
         }
 
         if (titles[newPage]) {
             addClass(titles[newPage], 'selected');
+            $('body,html').animate({
+                scrollTop : 0                       // Scroll to top of body
+            }, 10);
         }
 
         currentPageName = newPage;
     }
 }
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('.return-to-top').fadeIn(300);    // Fade in the arrow
+    } else {
+        $('.return-to-top').fadeOut(300);   // Else fade out the arrow
+    }
+});
+$('.return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
 
 function toggleClass(element, cls) {
     if (element.classList.contains(cls)) {
